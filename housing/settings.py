f206e19aa9a2d728792692c5bf1f81267da24a10
abimon <eddyabby59@gmail.com>
2024-09-15 17:54:32 +0300

    database

--- housing/settings.py
+++ housing/settings.py
@@ -68,22 +68,22 @@ WSGI_APPLICATION = 'housing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         # 'ENGINE': 'django.db.backends.mysql',
-#         # 'NAME': 'apektech_property',
-#         # 'USER':'apektech_admin',
-#         # 'PASSWORD':'Apek@2024',
-#         # 'HOST':'localhost'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apektech_property',
+        'USER':'apektech_admin',
+        'PASSWORD':'Apek@2024',
+        'HOST':'localhost'
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
